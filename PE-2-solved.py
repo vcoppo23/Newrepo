76bb55b34188f29e12233bd1fcf38df05630f479
vcoppo23 <vcoppo23@bsmschool.org>
2020-10-01 18:49:43 -0500

    PE problem 2

---
+++ PE-2-solved.py
@@ -0,0 +1,17 @@
+# Project Euler Problem #2 - Val Coppo
+
+# By considering the terms in the Fibonacci sequence whose values do not exceed
+# four million, find the sum of the even-valued terms.
+
+# Variables
+x = 0
+y = 1
+ans = 0
+while x <= 4000000:
+    
+    y = x + y     #Fibinachi Formula
+    x = y - x
+
+    if y % 2 == 0: #checks if even term
+        ans += y
+print ("The sum of even valued Fibonacci numbers up to 4 million is:\n", ans, sep = "")

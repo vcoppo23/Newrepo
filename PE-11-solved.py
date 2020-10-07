dc55cce9efc9504fc6bbaa9812e72fca638f6a1a
vcoppo23 <vcoppo23@bsmschool.org>
2020-10-07 09:24:37 -0500

    PE 11-12

---
+++ PE-11-solved.py
@@ -0,0 +1,8 @@
+# Project Euler Problem #11 - Val Coppo
+# Largest product in a grid
+# What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+
+
+# Variables
+
+
---
+++ PE-12-solved.py
@@ -0,0 +1,16 @@
+# Project Euler Problem #12 - Val Coppo
+# Highly divisible Triangle Number
+# What is the value of the first triangle number to have over five hundred divisors?
+
+
+# Variables
+
+add = 0
+
+def tri_nums(n):
+    add += n
+    return add
+
+
+
+print (tri_nums(range(1,7)))

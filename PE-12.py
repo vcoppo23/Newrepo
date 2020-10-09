738c44e3abf53cbfa00aff435f0666c18d3c4fb6
vcoppo23 <vcoppo23@bsmschool.org>
2020-10-09 09:39:21 -0500

    PE 12 fixes pt 2

--- PE-12.py
+++ PE-12.py
@@ -7,44 +7,27 @@
 import math
 add = 0
 i = 0
-divisors = [0]
+
 
 def multiple_counter(n):
     x = 0
     for i in range(1,int(math.sqrt(n))):
         if n % i == 0:
             x += 1
-    if x > 4:
-        x = 0
-        for b in range (int(n//2)):
-            if n % i == 0:
-                 x += 1
-        else:
-            return False
-                
-        return x + 2
-print (multiple_counter(3))
+    return x + 3
+
+
 def tri_num(m):
     t = m*(m+1)/2
-    #print (t)
     return multiple_counter(t)
-    
-for i in range (50000):
-    #print (tri_num(i), i)
-    
-    if tri_num(i) >= 10:
-        print (tri_num(i), i*(i+1)/2)
-        break
+
+def tri_num_check(b):
+    return b*(b+1)/2
 
 
-
-#while max(set(divisors)) < 500:
-
-  #  i += 1
-   # add = add + i
-   # divisors = []
-   # divisors.append(multiple_counter(add))
-    #print( max(set(divisors)),  add, i) # this is my debugger
-
-#print (add,max(set(divisors)))
-
+while tri_num(add) < 100:
+    #print (tri_num(add), tri_num_check(add))
+    tri_num(add)
+    add += 1
+print (tri_num_check(add))
+print (multiple_counter(64))

6ab2b9c2f1f2ee9f80e41fe15a2b2afb3a85966f
vcoppo23 <vcoppo23@bsmschool.org>
2020-10-08 23:42:56 -0500

    PE 12 Progress

--- PE-12.py
+++ PE-12.py
@@ -1,25 +1,50 @@
-# Project Euler Problem #12 - Val Coppo
+# Project Euler Problem #12 
 # Highly divisible Triangle Number
 # What is the value of the first triangle number to have over five hundred divisors?
 
 
 # Variables
 import math
-add = 1
-multiples = []
+add = 0
+i = 0
+divisors = [0]
 
-
-
-
-
-def factors(x):
-    for n in range(1, x + 1):
-        if x % n == 0:
-            return x
+def multiple_counter(n):
+    x = 0
+    for i in range(1,int(math.sqrt(n))):
+        if n % i == 0:
+            x += 1
+    if x > 4:
+        x = 0
+        for b in range (int(n//2)):
+            if n % i == 0:
+                 x += 1
         else:
-            return None
-for i in range(7):
-    add += i
-    print (add - 1)
-    multiples.append(factors(add))
-    print(multiples)
+            return False
+                
+        return x + 2
+print (multiple_counter(3))
+def tri_num(m):
+    t = m*(m+1)/2
+    #print (t)
+    return multiple_counter(t)
+    
+for i in range (50000):
+    #print (tri_num(i), i)
+    
+    if tri_num(i) >= 10:
+        print (tri_num(i), i*(i+1)/2)
+        break
+
+
+
+#while max(set(divisors)) < 500:
+
+  #  i += 1
+   # add = add + i
+   # divisors = []
+   # divisors.append(multiple_counter(add))
+    #print( max(set(divisors)),  add, i) # this is my debugger
+
+#print (add,max(set(divisors)))
+
